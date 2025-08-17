f4fd1680696eccc529a53b0b65f673762d958669
darenmuhammad <darenmuhammad10@gmail.com>
2025-08-17 15:03:20 +0700

    basic routing and useParams

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Vans from "./pages/Vans";
+import VanDetail from "./pages/VanDetail";
+import "./server";
 
 function App() {
     return (
@@ -11,11 +14,14 @@ function App() {
                 </Link>
                 <nav>
                     <Link to="/about">About</Link>
+                    <Link to="/vans">Vans</Link>
                 </nav>
             </header>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
+                <Route path="/vans" element={<Vans />} />
+                <Route path="/vans/:id" element={<VanDetail />} />
             </Routes>
         </BrowserRouter>
     );
