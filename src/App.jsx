ec9eabc119f9e40aafc48d49cd3366896f8e4d09
darenmuhammad <darenmuhammad10@gmail.com>
2025-08-19 18:35:40 +0700

    adding details on host pages and creating nested layout

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ import VanDetail from "./pages/VanDetail";
 import Layout from "./components/Layout";
 import Dashboard from "./pages/Host/Dashboard";
 import Income from "./pages/Host/Income";
+import HostVans from "./pages/Host/HostVans";
+import HostVansDetail from "./pages/Host/HostVanDetail";
+import HostVanInfo from "./pages/Host/HostVanInfo";
+import HostVanPhotos from "./pages/Host/HostVanPhotos";
+import HostVanPricing from "./pages/Host/HostVanPricing";
 import Reviews from "./pages/Host/Reviews";
 import HostLayout from "./components/HostLayout";
 
@@ -23,6 +28,15 @@ function App() {
                     <Route path="host" element={<HostLayout />}>
                         <Route index element={<Dashboard />} />
                         <Route path="income" element={<Income />} />
+                        <Route path="vans" element={<HostVans />} />
+                        <Route path="vans/:id" element={<HostVansDetail />}>
+                            <Route index element={<HostVanInfo />} />
+                            <Route
+                                path="pricing"
+                                element={<HostVanPricing />}
+                            />
+                            <Route path="photos" element={<HostVanPhotos />} />
+                        </Route>
                         <Route path="reviews" element={<Reviews />} />
                     </Route>
                 </Route>
